cd39c5a950a11e107a0753566e1abaf2708e775a
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2022-10-24 15:23:49 -0500

    Sets up project, SPM dependencies. initial commit

---
+++ FetchDesserts/MainTabBarController.swift
@@ -0,0 +1,11 @@
+//
+//  MainTabBarController.swift
+//  FetchDesserts
+//
+//  Created by joshua kaunert on 10/24/22.
+//
+
+import UIKit
+import iDoDeclare
+
+class MainTabViewController: UITabBarController {}
