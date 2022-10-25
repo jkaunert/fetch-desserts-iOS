2ed1c1bffebfbb582f04d4c2ad485c2770e5ac17
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2022-10-25 02:42:08 -0500

    Adds basic tests, remove main storyboard + refs

--- FetchDessertsTests/FetchDessertsTests.swift
+++ FetchDessertsTests/FetchDessertsTests.swift
@@ -1,36 +1,49 @@
-//
-//  FetchDessertsTests.swift
-//  FetchDessertsTests
-//
-//  Created by joshua kaunert on 10/24/22.
-//
-
 import XCTest
 @testable import FetchDesserts
+@testable import AsyncNet
 
 final class FetchDessertsTests: XCTestCase {
+	
+	func testDessertServiceMock() async throws {
+		do {
+			let serviceMock = DessertServiceMock()
+			let responseMock = try await serviceMock.getMeals(categoryName: "Dessert")
+			XCTAssertEqual(responseMock.meals.first?.id, "53005")
+		}
+		catch {
+			XCTFail("The request should not fail")
+		}
+	}
+	
+	@MainActor func testMainViewControllerFetchData() async throws {
+		
+		let viewController = MealCollectionViewController()
+		
+		async let expectation = expectation(description: "Fetch data from service")
+		
+		viewController.mealService = DessertServiceMock()
+		let vc = viewController
+		let mealResponse = try await vc.fetchData(category: "Dessert")
+		
+		await expectation.fulfill()
+		
+		waitForExpectations(timeout: 3.0, handler: nil)
+		
+		XCTAssertEqual(mealResponse.meals.first?.id, "53005")
+	}
+}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+final class DessertServiceMock: Mockable, MealResponseServiceable, AsyncRequestable {
+	
+	typealias ResponseModel = Models.MealResponse
+	typealias DetailsResponseModel = Models.RecipeResponse
+	
+	func getMeals(categoryName: String) async throws -> Models.MealResponse {
+		return loadJSON(filename: "mock_dessert_response", type: ResponseModel.self)
+	}
+	
+	func getMealDetails(mealId: String) async throws -> Models.RecipeResponse {
+		return loadJSON(filename: "mock_desserts_category_response", type: DetailsResponseModel.self)
+		
+	}
 }
